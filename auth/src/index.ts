b073f7facc5d45edd317042ff9d4716dad1410f2
Kody Low <kodylow7@gmail.com>
2021-10-12 08:32:02 -0700

    started building error handling service

---
+++ auth/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+
+const app = express();
+app.use(express.json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler);
+
+app.listen(3000, () => console.log('listening on 3000'));
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be Valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 8, max: 20 })
+      .withMessage('Password must be between 8 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new Error('Invalid email or password');
+    }
+
+    const { email, password } = req.body;
+    console.log('Creating a user...');
+    throw new Error('Error connecting to DB');
+    res.send({});
+  }
+);
+
+export { router as signupRouter };
